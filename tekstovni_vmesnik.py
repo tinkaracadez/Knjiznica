85edbc790f2e529031d0e219d0d22f77eb29dca8
Tinkara Čadež <tinkara.cadez@gmail.com>
2020-08-26 16:15:52 +0200

    Dostop do modela le prek metod na Shrambi

--- tekstovni_vmesnik.py
+++ tekstovni_vmesnik.py
@@ -2,16 +2,16 @@ from model import Shramba
 
 #dokler testiram, v trenutno datoteko kopiram naslednje podatke:
 from datetime import date
-from model import Shramba, Vnos, Vrsta
+from model import Shramba
 
 moja_shramba = Shramba()
 
 knjige = moja_shramba.dodaj_vrsto('knjige')
 filmi = moja_shramba.dodaj_vrsto('filmi')
 
-Vnos(knjige, 'Krive so zvezde', 'John Green', 'ljubezenski roman', date(2019, 5, 5), '5')
-Vnos(knjige, 'Harry Potter', 'J. K. Rowling', 'znanstvena fantastika', date(2020, 1, 3), '5')
-Vnos(filmi, 'To all the boys', 'nek režiser', 'romatična komedija', date(2019, 6, 5), '4')
+moja_shramba.dodaj_vnos(knjige, 'Krive so zvezde', 'John Green', 'ljubezenski roman', date(2019, 5, 5), '5')
+moja_shramba.dodaj_vnos(knjige, 'Harry Potter', 'J. K. Rowling', 'znanstvena fantastika', date(2020, 1, 3), '5')
+moja_shramba.dodaj_vnos(filmi, 'To all the boys', 'nek režiser', 'romatična komedija', date(2019, 6, 5), '4')
 #konec začasnih podatkov, izbrisala bom, ko bo program končan
 #in se bo te podatke bralo iz datoteke
 
--- test.py
+++ test.py
@@ -1,15 +1,13 @@
 from datetime import date
-from model import Shramba, Vnos, Vrsta
+from model import Shramba
 
 moja_shramba = Shramba()
 
 knjige = moja_shramba.dodaj_vrsto('knjige')
 filmi = moja_shramba.dodaj_vrsto('filmi')
 
-Vnos(knjige, 'Krive so zvezde', 'John Green', 'ljubezenski roman', date(2019, 5, 5), '5')
-Vnos(knjige, 'Harry Potter', 'J. K. Rowling', 'znanstvena fantastika', date(2020, 1, 3), '5')
-Vnos(filmi, 'To all the boys', 'nek režiser', 'romatična komedija', date(2019, 6, 5), '4')
+moja_shramba.dodaj_vnos(knjige, 'Krive so zvezde', 'John Green', 'ljubezenski roman', date(2019, 5, 5), '4')
+moja_shramba.dodaj_vnos(knjige, 'Harry Potter', 'J. K. Rowling', 'znanstvena fantastika', date(2020, 1, 3), '5')
+moja_shramba.dodaj_vnos(filmi, 'To all the boys', 'nek režiser', 'romatična komedija', date(2019, 6, 5), '4')
 
-print(knjige)
-print(filmi)
 print(moja_shramba)
